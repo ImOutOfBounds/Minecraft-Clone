fea88a2b7d7027614a032e2bfef03b045dd24678
Backspac3_N00B <173277005+ImOutOfBounds@users.noreply.github.com>
2025-08-26 09:52:58 -0300

    a

--- src/main/java/com/minecraftclone/Main.java
+++ src/main/java/com/minecraftclone/Main.java
@@ -123,9 +123,7 @@ public class Main {
             if (pitch < -89.0f) pitch = -89.0f;
         });
     }
-
-
-    // --- loop(): substitua seu método por este ---
+    
     private void loop() {
         glClearColor(0.0f, 0.9f, 2.0f, 0.0f);
 
